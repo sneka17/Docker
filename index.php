77903950d7b6a5bc09e70c0a0d737ec065259752
sneka17 <55310558+sneka17@users.noreply.github.com>
2021-11-29 19:09:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,121 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Docker sample app</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
 <?php
-   ob_start();
-   session_start();
+   /*
+   Author: VINEET KUMAR
+    */
+   # var_dump(getenv('MY_ENVVAR'));
+   # $admin = getenv("mysql_db_name");
+   #    echo $admin;
+   define('DB_USERNAME',getenv('MYSQL_USERNAME'));
+   define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
+   define('DB_HOST',getenv('MYSQL_HOST'));
+   define('DB_PORT',getenv('MYSQL_PORT'));
+   define('DB_DATABASE',getenv('MYSQL_DATABASE'));
+   #$host = GETENV('MYSQL_HOST');
+   #$port = getenv('MYSQL_PORT');
+   #$db   = $_ENV['MYSQL_USERNAME'];
+   #$username = getenv('MYSQL_USERNAME');
+   #echo $username;
+   #$password = getenv('MYSQL_PASSWORD');
+   #echo $host."<\br>".$db."<\br>".DB_USERNAME."<\br>".$password;
+   $con = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   #$con = mysqli_connect(localhost,tests,tests,register);
+   // Check connection
+   if (mysqli_connect_errno())
+     {
+     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+	session_start();
+    // If form submitted, insert values into the database.
+    if (isset($_POST['username'])){
+		
+		$username = stripslashes($_REQUEST['username']); // removes backslashes
+		$username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
+		$password = stripslashes($_REQUEST['password']);
+		$password = mysqli_real_escape_string($con,$password);
+		
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
+		$result = mysqli_query($con,$query) or die(mysqli_error());
+		$rows = mysqli_num_rows($result);
+        if($rows==1){
+			$_SESSION['username'] = $username;
+			header("Location: index.php"); // Redirect user to index.php
+            }else{
+				echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+				}
+    }else{
 ?>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
 
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
+<br /><br />
+</div>
+<?php } ?>
 
-<html lang = "en">
-   
-   <head>
-      <title>Tutorialspoint.com</title>
-      <link href = "css/bootstrap.min.css" rel = "stylesheet">
-      
-      <style>
-         body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #ADABAB;
-         }
-         
-         .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-         
-         .form-signin .form-signin-heading,
-         .form-signin .checkbox {
-            margin-bottom: 10px;
-         }
-         
-         .form-signin .checkbox {
-            font-weight: normal;
-         }
-         
-         .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-         }
-         
-         .form-signin .form-control:focus {
-            z-index: 2;
-         }
-         
-         .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-         }
-         
-         .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-         }
-         
-         h2{
-            text-align: center;
-            color: #017572;
-         }
-      </style>
-      
-   </head>
-	
-   <body>
-      
-      <h2>Enter Username and Password</h2> 
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            if (isset($_POST['login']) && !empty($_POST['username']) 
-               && !empty($_POST['password'])) {
-				
-               if ($_POST['username'] == 'tutorialspoint' && 
-                  $_POST['password'] == '1234') {
-                  $_SESSION['valid'] = true;
-                  $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
-                  
-                  echo 'You have entered valid use name and password';
-               }else {
-                  $msg = 'Wrong username or password';
-               }
-            }
-         ?>
-      </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username = tutorialspoint" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
-         </form>
-			
-         Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
-      </div> 
-      
-   </body>
+
+</body>
 </html>
